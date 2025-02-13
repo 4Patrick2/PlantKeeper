c4e9b9432fb1592547c35e7e8609bcba39862625
Patrick Mørch <patrickmoerch@gmail.com>
2025-02-13 12:52:59 +0100

    Starting calendar

--- app/(tabs)/calander.tsx
+++ app/(tabs)/calander.tsx
@@ -1,14 +1,38 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
+import {Calendar, CalendarList, Agenda, LocaleConfig} from 'react-native-calendars';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 
 const Calander = () => {
-  return (
-    <View>
-      <Text>Calander</Text>
-      <StatusBar backgroundColor="#161622" style="light"/>
-    </View>
+  const [selected, setSelected] = useState('');
 
+  return (
+    <SafeAreaView className='h-full bg-primary'>
+      <Calendar
+        style={{
+          borderColor: "primary"
+        }}
+        theme={{
+          backgroundColor: '#161622',
+          calendarBackground: '#161622',
+          textSectionTitleColor: '#b6c1cd',
+          selectedDayBackgroundColor: '#00adf5',
+          selectedDayTextColor: '#ffffff',
+          todayTextColor: '#00adf5',
+          dayTextColor: '#2d4150',
+          textDisabledColor: '#dd99ee'
+        }}
+  
+        onDayPress={day => {
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
+        }}
+      />
+    </SafeAreaView>
   )
 }
 
